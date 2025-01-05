b2d923896d7de06be10ea97e44d8090582cd42a4
Wessi <weskakay@gmail.com>
2025-01-05 23:39:01 +0100

    update and edit showPage and pageContens
    - deleted all html and js files and write it into pageContens.js

--- scripts/navigation/pages/stempelkarten.js
+++
@@ -1,8 +0,0 @@
-function stempelkarten() {
-    return `
-        <h2>Stempelkarten</h2>
-        <p>Sammeln Sie Stempel bei jeder Bestellung und erhalten Sie exklusive Rabatte!</p>
-        <p>Aktueller Stempelstand: <span id="stamp-count">0</span> von 10</p>
-        <progress id="stamp-progress" value="0" max="10"></progress>
-    `;
-}
